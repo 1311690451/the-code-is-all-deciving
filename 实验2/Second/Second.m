f50f59353025a569ea91ea88e3241992af3a8f5b
1311690451 <1311690451@qq.com>
2020-05-12 19:35:40 +0800

    Update Second.m

--- 实验2/Second/Second.m
+++ 实验2/Second/Second.m
@@ -5,17 +5,20 @@
 K = 1;          ts = 4.00;                            %init
 num = [8*K];    den = [1 7 14 8*(1+K)];             %抽象形式
 A = Caculate_Ka(den ,8 ,ts);
+A(3) = 1.05;
 
 den(4) = 8 + 8 * A(3);
-
 num = A(3) * 8;
+
 [z ,p ,k] = tf2zp(num ,den);
 sys = zpk(z ,p ,k);
 
 sysn2 = tf(num ,den);
-[y ,t] = step(sysn2);
+t = 0 : 0.01 :12;
+[y ,t] = step(sysn2 ,t);
 B = analysis_sys(y ,t);
 pos = B(4);
+B(3)
 
 figure(1);
 pzmap(sys);
